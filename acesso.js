a821cf024e5e41baa1b20efcf6f25638eb19ab45
kingrtork <empireshop302@gmail.com>
2024-08-13 13:54:06 -0300

    Fazendo a criação de uma nova funcionalidade

--- acesso.js
+++ acesso.js
@@ -3,3 +3,9 @@ function cadastramento(telefone) {
 }
 
 //Usando o numero de telefone para poder se cadastrar
+
+function captarlogin(telefone) {
+    return "Seja Bem Vindo Novamente"
+}
+
+//criando uma opção para pegar login
